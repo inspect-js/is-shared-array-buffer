41cb41918c2cf423938c767ffc67fd352130f6d1
Jordan Harband <ljharb@gmail.com>
2024-02-20 09:52:36 -0800

    add types

--- test/index.js
+++ test/index.js
@@ -11,13 +11,14 @@ var isSharedArrayBuffer = require('..');
 test('isSharedArrayBuffer', function (t) {
 	t.equal(typeof isSharedArrayBuffer, 'function', 'is a function');
 
-	var nonSABs = v.primitives.concat(v.objects);
+	// @ts-expect-error TS sucks with concat
+	var nonSABs = [].concat(v.primitives, v.objects);
 	forEach(nonSABs, function (nonSAB) {
 		t.equal(isSharedArrayBuffer(nonSAB), false, inspect(nonSAB) + ' is not a SharedArrayBuffer');
 	});
 
 	t.test('actual SharedArrayBuffer instances', { skip: typeof SharedArrayBuffer === 'undefined' }, function (st) {
-		var sab = new SharedArrayBuffer();
+		var sab = new SharedArrayBuffer(0);
 
 		st.equal(isSharedArrayBuffer(sab), true, inspect(sab) + ' is a SharedArrayBuffer');
 
